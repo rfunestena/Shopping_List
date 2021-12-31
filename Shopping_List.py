31a78436bb5d5711ed50a1b6c9a178766642524d
Raul <funes077@gmail.com>
2021-12-31 12:55:58 +0000

    Json added

--- Shopping_List.py
+++ Shopping_List.py
@@ -5,49 +5,67 @@
 # output the sorted data showing the item and the price.
 
 # Here comes your imports
-
+import json
 # Here comes your (few) global variables
-myTup  = ()
-myList = []
+myList  = []
+myShoppingList = []
+myShoppingListFiltered = []
 
 # Here comes your function definitions
 
 def main():
     print("Welcome To Shopping List!")
-    EnterIngredients()
-    SortIngredients()
-    OutpIngredients()
+    rangeList    = EnterIngredients()
+    NewRangeList = SortIngredients(rangeList)
+    OutpIngredients(NewRangeList)
+    create_json()
 
 
 
 def EnterIngredients():
-    numItems  = 0
-    EndofList = "Yes"
-    while (EndofList != "No"):
+    #Initialize variables
+    numItems     = 0
+    EndofList    = "Yes"
+    while (EndofList != "no"):
         itemName     = input("Enter item name: ")
         itemCategory = input("Enter item Category: ")
-        itemQuantity = input("Enter item Quantity: ")
+        itemQuantity = int(input("Enter item Quantity: "))
         EndofList    = input("Do you want to add another item?:")
-        myTup        = (itemName, itemCategory, itemQuantity)
-        myList.append(myTup)
+        myList       = [itemName, itemCategory, itemQuantity]
+        myShoppingList.append(myList)
         numItems += 1
+    return numItems
 
 
-def SortIngredients():
-    #numItems = input("How many do you want?")
-    #for x in range(int(numItems)):
-    #    itemName  = input("Enter item name: ")
-    #    itemPrice = input("Enter item price:")
-    #    myTup     = (itemPrice, itemName)
-    #    myList.append(myTup)
-    myList.sort()
+def SortIngredients(rangeList):
+# Look through the list and add same items
+# that user has introduced at different recipes
+# set to zero one of the items once it has been added.
+    #Initialize variables
+    numItemsFinalList = 0
+    for x in range(int(rangeList)):
+        for i in range(int(rangeList)):
+            if ((myShoppingList[x][0] == myShoppingList[i][0]) and (i != x)):
+                myShoppingList[x][2] += myShoppingList[i][2]
+                myShoppingList[i][2] = 0
+                #print(myShoppingList[x])
+# Remove cells that are set to zero
+# And asign it to a new list                
+    for z in range(int(rangeList)):
+        if (myShoppingList[z][2] != 0):
+            myShoppingListFiltered.append(myShoppingList[z])
+            numItemsFinalList += 1
+    return numItemsFinalList
+        
 
+def OutpIngredients(NewRangeList):
+    print("Name, Category, Quantity")
+    for x in range(int(NewRangeList)): 
+        print(myShoppingListFiltered[x])
 
-def OutpIngredients():
-    print(myList)
-
-
-
+def create_json():
+    with open('/home/raul/Documents/Raul/Sw_Dev/Shopping_List/Shopping_List.txt','w') as file:
+        json.dump(myShoppingListFiltered, file)
 
 if __name__ == "__main__":
         main()
